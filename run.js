4d316c243bf08e52c86fed9c1234cb3763a4888a
Barath Vignarajah <moneyinthesky@gmail.com>
2023-03-30 19:54:36 +0100

    Moving GitHub request logic to back-end to optimise front-end performance / caching GitHub responses / minor refactoring

--- run.js
+++ run.js
@@ -1,6 +1,108 @@
-import express from 'express';
+import express from "express";
+import { Octokit } from "octokit";
+import NodeCache from "node-cache";
+import repos from "./static/repos.json" assert { type: "json" };
+import authors from "./static/authors.json" assert { type: "json" };
 
+const octokit = new Octokit();
 const app = express();
-app.use(express.static('static'));
 
-app.listen(3000, () => console.log('Dashboard running on http://localhost:3000'));
+const githubCache = new NodeCache({ checkperiod: 1, deleteOnExpire: false });
+let populating = false;
+
+const reviewStatuses = ['requested', 'unreviewed', 'reviewed', 'missing'];
+
+app.use(express.static("static"));
+app.get("/prs", async (req, res) => {
+    res.json(githubCache.get('prs'));
+});
+
+async function getPrsAndPopulateCache(repos, authors) {
+    if(!populating) {
+        populating = true;
+        console.log('Making GitHub request - ' + new Date().toLocaleString());
+        const rawPrs = await getPullRequests(repos, authors);
+        const prs = buildGridOuput(repos, authors, rawPrs);
+        githubCache.set('prs', prs, 60);
+        populating = false;
+    }
+}
+
+async function getPullRequests(repos, authors) {
+	const allPrs = [];
+	const reposForSearch = repos.map((repo) => `repo:${repo}`).join(" ");
+	const authorsForSearch = authors.map((author) => `author:${author.githubUser}`).join(" ");
+
+	let response;
+	let page = 1;
+	do {
+		response = await octokit.rest.search.issuesAndPullRequests({
+			q: `is:open ${reposForSearch} ${authorsForSearch}`,
+			per_page: 100,
+			page,
+		});
+		allPrs.push(...response.data.items);
+		page++;
+		wait(100);
+	} while (allPrs.length < response.data.total_count && page <= 5);
+
+	return allPrs;
+}
+
+function buildGridOuput(repos, authors, prs) {
+	return repos
+		.flatMap((repo) =>
+			authors.map((author) => {
+				let prForRepoAndAuthor = prs.find((pr) => pr.user.login === author.githubUser && getRepo(pr) === repo);
+				return {
+					author: author.name,
+					repository: repo.split("/")[1],
+					reviewStatus: getReviewStatus(prForRepoAndAuthor),
+					link: getUrl(prForRepoAndAuthor),
+				};
+			})
+		)
+		.sort((a, b) => reviewStatuses.indexOf(a.reviewStatus) - reviewStatuses.indexOf(b.reviewStatus));
+}
+
+async function wait(millis) {
+	return new Promise((resolve) => {
+		setTimeout(() => resolve(), millis);
+	});
+}
+
+function getRepo(pr) {
+	let components = pr.repository_url.split("/");
+	return `${components[components.length - 2]}/${components[components.length - 1]}`;
+}
+
+function getReviewStatus(pr) {
+	if (getUrl(pr) === "-") {
+		return "missing";
+	} else if (doesLabelExist(pr, "reviewed")) {
+		return "reviewed";
+	} else if (doesLabelExist(pr, "review requested")) {
+		return "requested";
+	} else {
+		return "unreviewed";
+	}
+}
+
+function getUrl(pr) {
+	return pr ? pr.html_url : "-";
+}
+
+function doesLabelExist(pr, label) {
+	return pr ? pr.labels.some((l) => l.name === label) : false;
+}
+
+githubCache.on('expired', async (key, value) => {
+    if(key === 'prs') {
+        await getPrsAndPopulateCache(repos, authors);
+    }
+});
+
+app.listen(3000, async () => {
+    await getPrsAndPopulateCache(repos, authors);
+    console.log("Dashboard running on http://localhost:3000")
+});
--- static/script.js
+++ static/script.js
@@ -1,21 +1,21 @@
-import { Octokit } from "https://cdn.skypack.dev/@octokit/rest";
 import { Grid, html } from "https://unpkg.com/gridjs?module";
-
-import repos from "./repos.json" assert { type: "json" };
-import authors from "./authors.json" assert { type: "json" };
-
-const octokit = new Octokit();
 
 window.onload = () => onLoad();
 window.updateSearch = updateSearch;
+
+const reviewStatuses = {
+	requested: '#FFF59D',
+	unreviewed: '#90CAF9',
+	reviewed: '#A5D6A7',
+	missing: '#EF9A9A'
+}
 
 let gridData;
 let grid;
 let statusChart;
 
 async function onLoad() {
-	const prs = await getPullRequests(repos, authors);
-	gridData = buildGridOuput(repos, authors, prs);
+	gridData = await getPullRequests();
 	grid = new Grid({
 		columns: [
 			{
@@ -31,7 +31,7 @@ async function onLoad() {
 			{ name: "Review Status", attributes: colorBasedOnStatus, formatter: reviewStatusCell },
 			{ name: "Link", attributes: colorBasedOnStatus },
 		],
-		data: gridData,
+		data: buildGridData(gridData),
 		sort: true,
 		search: false,
 		style: {
@@ -53,22 +53,19 @@ async function onLoad() {
 	statusChart = new Chart(ctx, {
 		type: "doughnut",
 		data: {
-			labels: ["requested", "reviewed", "unreviewed", "missing"],
+			labels: Object.keys(reviewStatuses),
 			datasets: [
 				{
 					label: "Status",
 					data: getChartData(gridData),
-					backgroundColor: [
-						'#FFF59D',
-						'#A5D6A7',
-						'#90CAF9',
-						'#EF9A9A'
-					],
+					backgroundColor: Object.values(reviewStatuses),
 					borderWidth: 1,
 				},
 			],
 		},
 		options: {
+			maintainAspectRatio: false,
+			responsive: true,
 			plugins: {
 				legend: {
 					display: false
@@ -83,20 +80,26 @@ async function onLoad() {
 	updateHeading(gridData);
 }
 
+function buildGridData(prs) {
+	return prs.map(pr => {
+		if(pr.link !== '-') {
+			pr.link = html(`<a href='${pr.html_url}' target='_blank' rel='noopener noreferrer'>Link</a>`);
+		}
+		return pr;
+	})
+}
+
 function updateHeading(gridData) {
-	document.getElementById('requests-number').innerText = gridData.filter(row => row.reviewStatus === 'review requested').length + ' Requests';
-	document.getElementById('reviewed-number').innerText = gridData.filter(row => row.reviewStatus === 'reviewed').length + ' Reviewed';
-	document.getElementById('unreviewed-number').innerText = gridData.filter(row => row.reviewStatus === 'unreviewed').length + ' Unreviewed';
-	document.getElementById('missing-number').innerText = gridData.filter(row => row.reviewStatus === 'missing').length + ' Missing';
+	Object.keys(reviewStatuses).forEach(status => {
+		const capitalisedStatus = status.charAt(0).toUpperCase() + status.slice(1);
+		document.getElementById(`${status}-number`).innerText = 
+			`${gridData.filter(row => row.reviewStatus === status).length} ${capitalisedStatus}`;
+	})
 }
 
 function getChartData(gridData) {
-	return [
-		gridData.filter(row => row.reviewStatus === 'review requested').length,
-		gridData.filter(row => row.reviewStatus === 'reviewed').length,
-		gridData.filter(row => row.reviewStatus === 'unreviewed').length,
-		gridData.filter(row => row.reviewStatus === 'missing').length,
-	];
+	return Object.keys(reviewStatuses)
+		.map(status => gridData.filter(row => row.reviewStatus === status).length);
 }
 
 function performSearch(value) {
@@ -129,37 +132,15 @@ function searchableCell(value) {
 	return html(`<a href="javascript:updateSearch('${value}')"><img src='search.png' height='15px'/></a> ${value}`);
 }
 
-async function getPullRequests(repos, authors) {
-	const allPrs = [];
-	const reposForSearch = repos.map((repo) => `repo:${repo}`).join(" ");
-	const authorsForSearch = authors.map((author) => `author:${author.githubUser}`).join(" ");
-
-	let response;
-	let page = 1;
-	do {
-		response = await octokit.rest.search.issuesAndPullRequests({
-			q: `is:open ${reposForSearch} ${authorsForSearch}`,
-			per_page: 100,
-			page,
-		});
-		allPrs.push(...response.data.items);
-		page++;
-		wait(100);
-	} while (allPrs.length < response.data.total_count && page <= 5);
-
-	return allPrs;
-}
-
-async function wait(millis) {
-	return new Promise((resolve) => {
-		setTimeout(() => resolve(), millis);
-	});
+async function getPullRequests() {
+	const response = await fetch('prs');
+	return await response.json();
 }
 
 function reviewStatusCell(cell, row) {
 	const statusToClassMap = {
 		reviewed: "bg-success",
-		"review requested": "bg-warning",
+		requested: "bg-warning",
 		missing: "bg-danger",
 		unreviewed: "bg-info",
 	};
@@ -167,56 +148,7 @@ function reviewStatusCell(cell, row) {
 }
 
 function colorBasedOnStatus(cell, row) {
-	let statusToColorMap = {
-		reviewed: "reviewed",
-		"review requested": "requested",
-		missing: "missing",
-		unreviewed: "unreviewed",
-	};
 	if (row) {
-		return { class: statusToColorMap[row.cells[2].data] };
+		return { class: row.cells[2].data };
 	}
-}
-
-function getRepo(pr) {
-	let components = pr.repository_url.split("/");
-	return `${components[components.length - 2]}/${components[components.length - 1]}`;
-}
-
-function getUrl(pr) {
-	return pr ? html(`<a href='${pr.html_url}' target='_blank' rel='noopener noreferrer'>Link</a>`) : "-";
-}
-
-function doesLabelExist(pr, label) {
-	return pr ? pr.labels.some((l) => l.name === label) : false;
-}
-
-function getReviewStatus(pr) {
-	if (getUrl(pr) === "-") {
-		return "missing";
-	} else if (doesLabelExist(pr, "reviewed")) {
-		return "reviewed";
-	} else if (doesLabelExist(pr, "review requested")) {
-		return "review requested";
-	} else {
-		return "unreviewed";
-	}
-}
-
-function buildGridOuput(repos, authors, prs) {
-	return repos
-		.flatMap((repo) =>
-			authors.map((author) => {
-				let prForRepoAndAuthor = prs.find((pr) => pr.user.login === author.githubUser && getRepo(pr) === repo);
-				return {
-					author: author.name,
-					repository: repo.split("/")[1],
-					reviewStatus: getReviewStatus(prForRepoAndAuthor),
-					reviewRequested: doesLabelExist(prForRepoAndAuthor, "review requested").toString(),
-					reviewed: doesLabelExist(prForRepoAndAuthor, "reviewed").toString(),
-					link: getUrl(prForRepoAndAuthor),
-				};
-			})
-		)
-		.sort((a, b) => (a.reviewStatus === "review requested" ? -1 : 1));
 }
